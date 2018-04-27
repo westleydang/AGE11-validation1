8c3f60c75ce111a449543ff75134c08189d30e05
westleydang <wtdang@gmail.com>
2018-04-26 17:00:09 -0700

    Cleaned up, added batch to kk

--- LoadAndProcess.R
+++ LoadAndProcess.R
@@ -15,9 +15,6 @@ library(reshape2)
 kk = read.csv(file="All AGE 10 series counts-03232018/Wes_SUBROI_CLN_STACKED.csv", header=TRUE, sep=",")
 kk$idBysubroi = as.factor(kk$idBysubroi)
 
-# Remove the non-transgenics
-k2 = kk %>% filter(! ID %in% c("8351", "8352"))
-
 
 # Merge animal details
 animal_details = read.csv("animal_details.csv")
@@ -26,29 +23,33 @@ colnames(animal_details)[9] = "idBysubroi"
 kk = merge(animal_details, kk, by = "idBysubroi")
 colnames(kk)[2] = "GROUP"
 
-
-
+# Remove the non-transgenics
+kk = kk %>% filter(! idBysubroi %in% c("8351", "8352"))
 
 # Merge group details
 group_details = read.csv("group_details.csv")
 names(group_details)[1] = "GROUP" # because for some reason it messes up
 kk = merge(group_details, kk, by="GROUP")
 
-replace = read.csv("replacements.csv")
-names(replace) = c("pre", "post") # because for some reason the names mess up
+# Replace the CHANNEL names
+replace_details = read.csv("replacements.csv")
+names(replace_details) = c("pre", "post") # because for some reason the names mess up
+names(kk)[15:19] = as.character(replace_details[1:5,2])
+
+# Normalize the OL counts
+kk$NormOL = kk$OL/(kk$ArcIHC + kk$H2BGFP - kk$OL)
 
 # Rename the age groups
-kk$EXPT = as.factor(as.character(replace$post[match(kk$EXPT, replace$pre)]))
+kk$EXPT = as.factor(as.character(replace_details$post[match(kk$EXPT, replace_details$pre)]))
 
-# normalize the OL counts
-kk$OLmm2.norm = kk$OLmm2
-kk$OLmm2.norm = kk$OLmm2/(kk$Ch2mm2 + kk$Ch4mm2 - kk$OLmm2)
-
-# Rename all variable names of the channels
-names(kk)[15:20] = as.character(replace[1:6,2])
+# Merge BATCH details
+batch_details = read.csv("batch_details.csv")
+kk$GRAIN = paste(kk$GROUP, kk$EXPT)
+colnames(batch_details) = c("GRAIN", "BATCH")
+kk = merge(kk, batch_details, by="GRAIN")
 
 attach(kk)
 
 # Transform rawkk to a long format
-kk.long = melt(kk, id.vars=names(kk[1:14]), variable.name = "CHANNEL", value.name="DENSITY")
+kk.long = melt(kk, id.vars=names(kk[c(1:14,22)]), variable.name = "CHANNEL", value.name="DENSITY")
 
