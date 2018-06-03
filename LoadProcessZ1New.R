bf6f9b2f1676cab4d42646baee87e68d0161a687
Unknown <w3zz3x@gmail.com>
2018-06-03 10:32:16 -0700

    made NormOL, excluded low pop means, refactored colnums
    Made a script that makes the NormOL channel, then also refactored the dataframe to include it, then copied the popmeans file where you can find which SubROI has low counts across all animals, and you can exclude that from the dataframe (knew.molten)

--- LoadProcessZ1New.R
+++ LoadProcessZ1New.R
@@ -5,35 +5,39 @@
 library(reshape2)
 library(dplyr)
 
-knew = read.csv("SubroiAGGR - NEW MAY29 WESMATLAB.csv")
-
+knew = read.csv("SubroiAGGR - jun 3.csv")
+source("MakeNormOL.R")
 # colnames(knew)[1] = "ID"
 # 
 # knew = knew %>% filter(! ID %in% c("8351", "8352")) # formerly MouseID
 
 # Melt the data, all measurements in one variable
-names.not.measurements = as.character(read.csv("OUTPUT/names_not_measurements_KK.csv", header=T)$x)
-names.measurements = as.character(read.csv("OUTPUT/names_measurements_KK.csv", header=T)$x)
+# names.not.measurements = as.character(read.csv("OUTPUT/names_not_measurements.csv", header=T)$x)
+# names.measurements = as.character(read.csv("OUTPUT/names_measurements.csv", header=T)$x)
 knew.molten = melt(knew, id.vars = names.not.measurements)
 
 # Display the variable levels (all the measurements)
 levels(knew.molten$variable)
-    
+
 # Combine all the transformations
-units.counts = levels(knew.molten$variable)[c(1:20,41:60)]
-units.density = levels(knew.molten$variable)[c(21:40)]
+units.counts = levels(knew.molten$variable)[c(1:24,49:72)]
+units.density = levels(knew.molten$variable)[c(25:48)]
+normalized.yes = levels(knew.molten$variable)[49:72]
+
+# write.csv(units.counts, "OUTPUT/units_count.csv")
+# write.csv(units.density, "OUTPUT/units_density.csv")
+# write.csv(normalized.yes, "OUTPUT/normalized_yes.csv")
 
 knew.molten = knew.molten %>%
-  mutate(UNITS = ifelse(variable %in% units.counts, "counts", "density"))
+  mutate(UNITS = ifelse(variable %in% units.counts, "counts", "density"),
+         NORMD = ifelse(variable %in% normalized.yes, "yes", "no")
+         )
 
-normalized.yes = levels(knew.molten$variable)[41:60]
-knew.molten = knew.molten %>%
-  mutate(NORMD = ifelse(variable %in% normalized.yes, "yes", "no"))
 
-brightness.low = levels(knew.molten$variable)[seq(1,60,4)]
-brightness.mid = levels(knew.molten$variable)[seq(2,60,4)]
-brightness.hi = levels(knew.molten$variable)[seq(3,60,4)]
-brightness.total = levels(knew.molten$variable)[seq(4,60,4)]
+brightness.low = names.measurements[seq(1,72,4)]
+brightness.mid = names.measurements[seq(2,72,4)]
+brightness.hi = names.measurements[seq(3,72,4)]
+brightness.total = names.measurements[seq(4,72,4)]
 
 knew.molten = knew.molten %>%
   mutate(BRIGHTNESS = 
@@ -49,6 +53,7 @@ chan2 = levels(knew.molten$variable)[grep("Ch2", levels(knew.molten$variable))]
 chan3 = levels(knew.molten$variable)[grep("Ch3", levels(knew.molten$variable))]
 chan4 = levels(knew.molten$variable)[grep("Ch4", levels(knew.molten$variable))]
 chan5 = levels(knew.molten$variable)[grep("OL", levels(knew.molten$variable))]
+chan6 = levels(knew.molten$variable)[grep("NormOL", levels(knew.molten$variable))]
 
 knew.molten = knew.molten %>%
   mutate(CHANNEL = 
@@ -57,7 +62,8 @@ knew.molten = knew.molten %>%
              variable %in% chan2 ~ "H2BGFP",
              variable %in% chan3 ~ "GFAP",
              variable %in% chan4 ~ "ArcIHC",
-             variable %in% chan5 ~ "OL"
+             variable %in% chan5 ~ "OL",
+             variable %in% chan6 ~ "NormOL"
            ))
 knew.molten$CHANNEL = as.factor(knew.molten$CHANNEL)
 
@@ -71,6 +77,18 @@ knew.molten = knew.molten %>%
            ))
 knew.molten$GRAIN = paste(knew$EXPT, knew$GROUP)
 
+# knew.molten$ID = as.factor(knew.molten$ID)
+# knew.molten$EXPT = as.factor(knew.molten$EXPT)
+# knew.molten$BRIGHTNESS = as.factor(knew.molten$BRIGHTNESS)
+
 
 rx = theme(axis.text.x = element_text(angle = 90, hjust = 1)) 
 channel.list = c("DAPI", "H2BGFP", "ArcIHC", "GFAP", "OL", "NormOL")
+
+# --- EXCLUDE SubROI with low representation across all mice
+
+pop.means = read.csv("SubroiPopMEANS - jun 3.csv")
+exclude.low.pop.means = pop.means$SubROI[pop.means$CountPerROI<0.5*max(pop.means$CountPerROI)]
+
+knew = knew[!(knew$SubROI_SubroiAGGR %in% exclude.low.pop.means),]
+knew.molten= knew.molten[!(knew.molten$SubROI_SubroiAGGR %in% exclude.low.pop.means),]
---
+++ MakeNormOL.R
@@ -0,0 +1,35 @@
+# creates the normalized OL variables
+
+ol = function(OL, A, B) {
+  NormOL = OL / (A + B - OL)
+  NormOL
+}  
+ol(.05, .2, .2)
+
+
+knew = knew %>%
+  mutate(
+    # counts, not normd
+    NormOLm05sd = ol(OLm05sd, Ch2m05sd, Ch4m05sd), 
+    NormOLm05sdm05sd = ol(OLm05sdm05sd, Ch2m05sdm05sd, Ch4m05sdm05sd),
+    NormOLm05sd1 = ol(OLm05sd1, Ch2m05sd1, Ch4m05sd1),
+    NormOLTotal = ol(OLTotal, Ch2Total, Ch4Total),
+    # densities
+    NormOLm05sdperMM2 = ol(OLm05sdperMM2, Ch2m05sdperMM2, Ch4m05sdperMM2), 
+    NormOLm05sdm05sdperMM2 = ol(OLm05sdm05sdperMM2, Ch2m05sdm05sdperMM2, Ch4m05sdm05sdperMM2),
+    NormOLm05sd1perMM2 = ol(OLm05sd1perMM2, Ch2m05sd1perMM2, Ch4m05sd1perMM2),
+    NormOLTotalperMM2 = ol(OLTotalperMM2, Ch2TotalperMM2, Ch4TotalperMM2),
+    # counts, yes normd
+    NormOLm05sd_COUNTnormd = ol(OLm05sd_COUNTnormd, Ch2m05sd_COUNTnormd, Ch4m05sd_COUNTnormd), 
+    NormOLm05sdm05sd_COUNTnormd = ol(OLm05sdm05sd_COUNTnormd, Ch2m05sdm05sd_COUNTnormd, Ch4m05sdm05sd_COUNTnormd),
+    NormOLm05sd1_COUNTnormd = ol(OLm05sd1_COUNTnormd, Ch2m05sd1_COUNTnormd, Ch4m05sd1_COUNTnormd),
+    NormOLTotal_COUNTnormd = ol(OLTotal_COUNTnormd, Ch2Total_COUNTnormd, Ch4Total_COUNTnormd)
+  )
+
+refactored.col = names(knew)[c(1:38, 86:89, 39:65, 90:93, 66:85, 94:97)]
+# write.csv(refactored.col, "OUTPUT/refactored_columns_for_knew.csv")
+knew = knew[, refactored.col]
+
+# define the names
+names.not.measurements = refactored.col[c(1:18, 43:49)]
+names.measurements = refactored.col[c(19:42, 50:97)]
