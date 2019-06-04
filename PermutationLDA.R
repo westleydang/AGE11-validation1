49a536c2a40a75aff4d8d0566deb6487b33f33a2
Westley Dang <wtdang@gmail.com>
2019-06-03 21:56:17 -0700

    made sure it's all LinearDA and not SVM

--- PermutationLDA.R
+++ PermutationLDA.R
@@ -3,24 +3,39 @@ library(PredPsych)
 
 # trying to do LDA with cross validation
 
+data.for.lda = gfp %>%
+  ddply(.(System), transform, z = scale(weighted)) %>%
+  dcast(ID ~ System, value.var="z", fun.aggregate = mean, .drop=T) 
+
+data.for.lda = merge(data.for.lda, animal_details %>% 
+                       # filter(EXPT=='OLD') %>%
+                       mutate(GRAIN = paste(EXPT, VALENCE)) %>% 
+                       dplyr::select(ID, GRAIN), by="ID")
+
+# remove spaces? 
+names(data.for.lda) = gsub(" ", "_", names(data.for.lda))
+names(data.for.lda) = gsub("-", ".", names(data.for.lda))
+
+# impute data
+data.for.lda = mice::complete(mice(data.for.lda, m=5, seed=25))
+
 data.fl = data.for.lda %>%
   mutate(factor = case_when(
     GRAIN == 'AGE 10 SHOCK' ~ "y-shk",
     GRAIN == 'AGE 11 SHOCK' ~ "o-shk",
     GRAIN %in% c('AGE 10 CONTEXT', 'AGE 11 CONTEXT') ~ "else"
-    
   ))
 
 data.fl = data.for.lda %>%
   mutate(factor = GRAIN)
-)
+
 
 LDAModel <- LinearDA(Data = data.fl, classCol = 17, 
                      selectedCols = c(2:15,17), cvType="holdout",
-                     cvFraction = 0.5)
+                     cvFraction = 0.75)
 
 PermutationResult <- ClassPerm(Data = data.fl, classCol = 17, 
-                               selectedCols = c(2:15,17), nSims = 100, cvType = "holdout",
+                               selectedCols = c(2:17), nSims = 100, cvType = "holdout",
                                cvFraction = 0.80)
 
 
